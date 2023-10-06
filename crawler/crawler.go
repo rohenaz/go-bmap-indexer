95d34b22df70bde315dde8d6c456281e52d05a28
David Case <david@fyxgaming.com>
2023-10-06 16:02:39 -0400

    Add inputs and outputs to data

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -235,6 +235,8 @@ func processTx(bmapData *bmap.Tx) {
 		"_id": bmapData.Tx.Tx.H,
 		"tx":  bmapData.Tx.Tx,
 		"blk": bmapData.Tx.Blk,
+		"in":  bmapData.Tx.In,
+		"out": bmapData.Tx.Out,
 	}
 
 	if bmapData.AIP != nil {
@@ -262,7 +264,9 @@ func processTx(bmapData *bmap.Tx) {
 		return
 	}
 	bsonData["MAP"] = bmapData.MAP
-	if _, ok := bmapData.MAP[0]["type"].(string); !ok {
+	if collection, ok := bmapData.MAP[0]["type"].(string); ok {
+		bsonData["collection"] = collection
+	} else {
 		// log.Println("Error: MAP 'type' key does not exist.")
 		return
 	}
