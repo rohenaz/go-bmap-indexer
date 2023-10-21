d5967ac05147d13da8e4da21ee9da9f09647c014
Luke Rohenaz <rohenaz@gmail.com>
2023-10-21 05:27:25 -0400

    get all the old stuff

--- config/config.go
+++ config/config.go
@@ -5,7 +5,7 @@ const (
 	SkipSPV           = true
 	SubscriptionID    = "5af4235fe3e2a36965a46805a10dd48e0d659467c7f5df0a8c48ba5d32e406dd"
 	MinerAPIEndpoint  = "https://mapi.gorillapool.iom/mapi/tx/"
-	FromBlock         = 800000
+	FromBlock         = 400000
 	BockSyncRetries   = 5    // number of retries before block is marked failed
 	DeleteAfterIngest = true // delete json data files after ingesting to db
 )
