4294e24bcb13de6316fdb348b9912626786f9474
Luke Rohenaz <rohenaz@gmail.com>
2023-10-24 17:28:44 -0400

    update jb

--- config/config.go
+++ config/config.go
@@ -5,8 +5,8 @@ const (
 	SkipSPV           = true
 	SubscriptionID    = "5af4235fe3e2a36965a46805a10dd48e0d659467c7f5df0a8c48ba5d32e406dd"
 	MinerAPIEndpoint  = "https://mapi.gorillapool.iom/mapi/tx/"
-	JunglebusEndpoint = "https://hetzner.junglebus.gorillapool.io/"
-	FromBlock         = 574287 // "Welcome to the Future" post = 574287
-	BockSyncRetries   = 5      // number of retries before block is marked failed
-	DeleteAfterIngest = true   // delete json data files after ingesting to db
+	JunglebusEndpoint = "https://germany.junglebus.gorillapool.io/" // "https://hetzner.junglebus.gorillapool.io/"
+	FromBlock         = 574287                                      // "Welcome to the Future" post = 574287
+	BockSyncRetries   = 5                                           // number of retries before block is marked failed
+	DeleteAfterIngest = true                                        // delete json data files after ingesting to db
 )
