34cee45ee15abdb4107caacd2be97ff1c1351be1
Jason Chavannes <jason.chavannes@gmail.com>
2025-01-30 07:01:21 -0800

    Don't worry about tx counter on fatal.

--- crawler/eventListener.go
+++ crawler/eventListener.go
@@ -26,8 +26,7 @@ func eventListener(subscription *junglebus.Subscription) {
 		case "status":
 			switch event.Status {
 			case "disconnected":
-				txCount = 0
-				log.Fatalf("%sDisconnected from Junglebus. Reset tx counter.%s\n", chalk.Green, chalk.Reset)
+				log.Fatalf("%sDisconnected from Junglebus.%s\n", chalk.Green, chalk.Reset)
 			case "connected":
 				log.Printf("%sConnected to Junglebus%s\n", chalk.Green, chalk.Reset)
 
